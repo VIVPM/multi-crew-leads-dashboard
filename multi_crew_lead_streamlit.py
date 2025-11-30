8f2ddce2bc5a6c43b963cce7c1b9750a46003050
VIVPM <vivpm99@gmail.com>
2025-11-30 11:50:25 +0530

    fix Langfuse integration

--- multi_crew_lead_streamlit.py
+++ multi_crew_lead_streamlit.py
@@ -235,7 +235,7 @@ if not sambana_key:
 # =========================
 # LLM & Agents
 # =========================
-llm3 = LLM(model="sambanova/Meta-Llama-3.3-70B-Instruct", api_key=sambana_key)
+llm3 = LLM(model="sambanova/DeepSeek-V3.1-Terminus", api_key=sambana_key)
 
 lead_data_agent = Agent(
     config=lead_agents_config['lead_data_agent'],
